2f726b22006c78f3d1093ddaa9f568af5324d7bf
Xie-Muxi <mx-xie@outlook.com>
2024-04-14 23:14:02 +0800

    rm useless files

--- projects/OneFormer/oneformer/__init__.py
+++
@@ -1,7 +0,0 @@
-from .datasets.loading import PreprocessAnnotationsOneFormer
-from .oneformer_head import OneFormerHead
-from .oneformer import OneFormer
-
-__all__ = [
-    'PreprocessAnnotationsOneFormer', 'OneFormerHead', 'OneFormer'
-]
--- rsseg/datasets/__init__.py
+++ rsseg/datasets/__init__.py
@@ -2,6 +2,8 @@ from .datasets import WiderFaceDataset
 from .transformers import *
 from .aerorit import AeroRITDataSet
 from .nwpu import NWPUInsSegDataset
+from .isaid import iSAIDDataset
+from .potsdam import PotsdamDataset
 
 __all__ = ['WiderFaceDataset', 'RetinaFacePipeline',
-           'AeroRITDataSet', 'NWPUInsSegDataset']
+           'AeroRITDataSet', 'NWPUInsSegDataset', 'iSAIDDataset', 'PotsdamDataset']
